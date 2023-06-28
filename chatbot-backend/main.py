bca587f39b978c514db4132bd24d4180eec424a3
codegzy-rabbit <2570713180@qq.com>
2023-06-28 23:43:34 +0800

    chatbot

---
+++ chatbot-backend/main.py
@@ -0,0 +1,87 @@
+from fastapi import FastAPI, HTTPException, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+import openai
+from decouple import config
+from fastapi.responses import StreamingResponse
+
+from functions.text_to_speech import convert_text_to_speech
+from functions.openai_requests import convert_audio_to_text, get_chat_response
+from functions.database import store_messages, reset_messages
+
+app = FastAPI()
+
+# Get Environment Vars
+openai.organization = config("OPEN_AI_ORG")
+openai.api_key = config("OPEN_AI_KEY")
+
+
+# Initiate App
+app = FastAPI()
+
+# CORS - Origins
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:4173",
+    "http://localhost:3000",
+]
+
+
+# CORS - Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Reset Conversation
+@app.get("/reset")
+async def reset_conversation():
+    reset_messages()
+    return {"response": "conversation reset"}
+
+
+# Post bot response
+# Note: Not playing back in browser when using post request.
+@app.post("/post-audio/")
+async def post_audio(file: UploadFile = File(...)):
+
+    # Convert audio to text - production
+    # Save the file temporarily
+    with open(file.filename, "wb") as buffer:
+        buffer.write(file.file.read())
+    audio_input = open(file.filename, "rb")
+
+    # Decode audio
+    message_decoded = convert_audio_to_text(audio_input)
+
+    # Guard: Ensure output
+    if not message_decoded:
+        raise HTTPException(status_code=400, detail="Failed to decode audio")
+
+    # Get chat response
+    chat_response = get_chat_response(message_decoded)
+
+    # Store messages
+    store_messages(message_decoded, chat_response)
+
+    # Guard: Ensure output
+    if not chat_response:
+        raise HTTPException(status_code=400, detail="Failed chat response")
+
+    # Convert chat response to audio
+    audio_output = convert_text_to_speech(chat_response)
+
+    # Guard: Ensure output
+    if not audio_output:
+        raise HTTPException(status_code=400, detail="Failed audio output")
+
+    # Create a generator that yields chunks of data
+    def iterfile():
+        yield audio_output
+
+    # Use for Post: Return output audio
+    return StreamingResponse(iterfile(), media_type="application/octet-stream")
---
+++ chatbot-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import Controller from "./components/Controller.tsx";
+
+function App() {
+
+  return (
+    <div>
+      <Controller />
+    </div>
+  )
+}
+
+export default App
